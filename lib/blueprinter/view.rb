1a025d831fb8f0bb5af070ea8b3b686b619e1628
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2017-10-30 13:53:11 -0700

    Ensure view works with unit tests.
    Put the defaults for a View into keyword arg defaults. I think this gives
    us a lot more flexibility in various possible use cases as well as in
    testing.
    
    I caught a bug in 'def <<' in which we were keying the early escape off of
    whether or nor the field was a key in the @fields hash, where we should
    have been looking for field.name.
    
    Lastly, I removed the unnecessary instance variables since they were
    already attr_readers.

--- lib/blueprinter/view.rb
+++ lib/blueprinter/view.rb
@@ -1,29 +1,29 @@
 module Blueprinter
   # @api private
   class View
-    attr_reader :fields, :included_view_names, :excluded_field_names
+    attr_reader :excluded_field_names, :fields, :included_view_names, :name
 
-    def initialize(name)
+    def initialize(name, fields: {}, included_view_names: [], excluded_view_names: [])
       @name = name
-      @fields = {}
-      @included_view_names = []
-      @excluded_field_names = []
+      @fields = fields
+      @included_view_names = included_view_names
+      @excluded_field_names = excluded_view_names
     end
 
     def include_view(view_name)
-      @included_view_names << view_name
+      included_view_names << view_name
     end
 
     def exclude_field(field_name)
-      @excluded_field_names << field_name
+      excluded_field_names << field_name
     end
 
     def <<(field)
-      if @fields.has_key? field
+      if fields.has_key?(field.name)
         raise BlueprinterError,
-          "Field #{field.name} already defined on #{@name}"
+          "Field #{field.name} already defined on #{name}"
       end
-      @fields[field.name] = field
+      fields[field.name] = field
     end
   end
 end

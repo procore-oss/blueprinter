8e921b03d66478c3c467c0c9fcc8a64d24d911aa
Philip Nguyen <philip.nguyen@procore.com>
2018-01-04 16:01:49 -0800

    Use in memory sqlite3 database.
    This will load the schema into memory and run the tests from an in
    memory database. This way we do not have to have the sqlite3 db file
    prepared.

--- spec/rails_test_helper.rb
+++ spec/rails_test_helper.rb
@@ -1,3 +1,4 @@
 require File.expand_path("../../spec/dummy/config/environment.rb", __FILE__)
 Rails.env = 'test'
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
+load 'dummy/db/schema.rb'

7984f4e78bb0be00870ac3e0535eee2fcb3cc56c
Vinaya L Shrestha <vinaya.shrestha@procore.com>
2018-05-15 08:50:22 -0700

    Updated return logic. Version update

--- lib/blueprinter/extractors/association_extractor.rb
+++ lib/blueprinter/extractors/association_extractor.rb
@@ -1,8 +1,8 @@
 module Blueprinter
   class AssociationExtractor < Extractor
     def extract(association_name, object, local_options, options={})
-      value = object.public_send(association_name) || options[:default]
-      return value if value.blank?
+      value = object.public_send(association_name)
+      return (value || options[:default]) if value.nil?
       view = options[:view] || :default
       options[:blueprint].prepare(value, view_name: view, local_options: local_options)
     end

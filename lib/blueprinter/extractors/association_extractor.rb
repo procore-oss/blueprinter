e8c4faa5cc3f3758f7b33c7e0a24d957c264790b
Michael Clayton <michael.clayton@procore.com>
2018-11-13 14:03:45 -0800

    Minor cleanup

--- lib/blueprinter/extractors/association_extractor.rb
+++ lib/blueprinter/extractors/association_extractor.rb
@@ -6,12 +6,11 @@ module Blueprinter
 
     def extract(association_name, object, local_options, options={})
       if options.key?(:default)
-        default = options.delete(:default)
-        value = @extractor.extract(association_name, object, local_options, options)
-        return default if value.nil?
+        value = @extractor.extract(association_name, object, local_options, options.except(:default))
+        return options[:default] if value.nil?
       else
         value = @extractor.extract(association_name, object, local_options, options)
-        return default if value.nil?
+        return options[:default] if value.nil?
         view = options[:view] || :default
         options[:blueprint].prepare(value, view_name: view, local_options: local_options)
       end

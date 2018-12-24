2d3bae648938799a84e2e1df9888beeaab707683
Ritikesh <ritikeshsisodiya@gmail.com>
2018-12-24 12:12:18 +0530

    cosmetic changes

--- lib/blueprinter/extractors/association_extractor.rb
+++ lib/blueprinter/extractors/association_extractor.rb
@@ -8,14 +8,14 @@ module Blueprinter
       value = @extractor.extract(association_name, object, local_options, options.except(:default))
       return options[:default] if value.nil?
       view = options[:view] || :default
-      association_blueprint(value, options).prepare(value, view_name: view, local_options: local_options)
+      blueprint = association_blueprint(options[:blueprint], value)
+      blueprint.prepare(value, view_name: view, local_options: local_options)
     end
     
     private
 
-    def association_blueprint(value, options)
-      return options[:blueprint] unless options[:blueprint].is_a?(Proc)
-      options[:blueprint].call(value)
+    def association_blueprint(blueprint, value)
+      blueprint.is_a?(Proc) ? blueprint.call(value) : blueprint
     end
   end
 end

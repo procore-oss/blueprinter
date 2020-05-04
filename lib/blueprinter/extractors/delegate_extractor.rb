924e3908725f1bc3861280405c5928c573a1519f
Khoa Nguyen <thanhkhoait@gmail.com>
2020-05-04 14:16:30 +0700

    [Refactor] Change to public_send

--- lib/blueprinter/extractors/delegate_extractor.rb
+++ lib/blueprinter/extractors/delegate_extractor.rb
@@ -6,10 +6,10 @@ module Blueprinter
     def extract(field_name, object, _local_options, options = {})
       delegate_options = options[:delegate]
       raise(DelegateArgumentError, "#{field_name}: delegate[:to] need to define") unless delegate_options.has_key?(:to)
-      delegated_object = object.send(delegate_options[:to])
+      delegated_object = object.public_send(delegate_options[:to])
       return nil if delegated_object.nil?
       raise(DelegateArgumentError, "#{field_name}: delegate[:source] need to define") unless delegate_options.has_key?(:source)
-      delegated_object.send(delegate_options[:source])
+      delegated_object.public_send(delegate_options[:source])
     end
   end
 end

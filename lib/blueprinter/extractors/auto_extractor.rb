48fbda2eb3f36a943960cf7a69178b7829e8a0af
Midhun Thimmapuram <midhun.thimmapuram@freshworks.com>
2022-05-25 22:49:14 +0530

    initialise configurations

--- lib/blueprinter/extractors/auto_extractor.rb
+++ lib/blueprinter/extractors/auto_extractor.rb
@@ -8,6 +8,8 @@ module Blueprinter
       @public_send_extractor = PublicSendExtractor.new
       @block_extractor = BlockExtractor.new
       @datetime_formatter = DateTimeFormatter.new
+      @config_datetime_format = Blueprinter.configuration.datetime_format
+      @config_field_default = Blueprinter.configuration.field_default
     end
 
     def extract(field_name, object, local_options, options = {})
@@ -19,11 +21,11 @@ module Blueprinter
     private
     
     def format_datetime?(options)
-      options.key?(:datetime_format) || Blueprinter.configuration.datetime_format
+      options.key?(:datetime_format) || @config_datetime_format
     end
 
     def default_value(field_options)
-      field_options.key?(:default) ? field_options.fetch(:default) : Blueprinter.configuration.field_default
+      field_options.key?(:default) ? field_options.fetch(:default) : @config_field_default
     end
 
     def extractor(object, options)

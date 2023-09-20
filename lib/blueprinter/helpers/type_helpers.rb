674f33643aa1c659cff084655b975dac056010c4
Jacob Sheehy <jacobjsheehy@gmail.com>
2023-09-19 20:05:29 -0400

    [refactor] Determine if object is array-like by checking if it responds to #to_ary
    Signed-off-by: Jacob Sheehy <jacobjsheehy@gmail.com>

--- lib/blueprinter/helpers/type_helpers.rb
+++ lib/blueprinter/helpers/type_helpers.rb
@@ -1,13 +1,9 @@
 module Blueprinter
   module TypeHelpers
     private
-    def active_record_relation?(object)
-      !!(defined?(ActiveRecord::Relation) &&
-        object.is_a?(ActiveRecord::Relation))
-    end
 
     def array_like?(object)
-      object.is_a?(Array) || active_record_relation?(object)
+      object.is_a?(Array) || object.respond_to?(:to_ary)
     end
   end
 end

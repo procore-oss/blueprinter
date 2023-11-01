e9fbae94b1f32abf4a18163993078fe55042dca7
Jacob Sheehy <jacobjsheehy@gmail.com>
2023-10-31 20:39:22 -0400

    [refactor] Rely on inclusion of Enumerable to determine if object can function as an Array
    Signed-off-by: Jacob Sheehy <jacobjsheehy@gmail.com>

--- lib/blueprinter/helpers/type_helpers.rb
+++ lib/blueprinter/helpers/type_helpers.rb
@@ -4,9 +4,15 @@ module Blueprinter
   module TypeHelpers
     private
 
+    # Returns true if object can act as an Array. When `array_like_classes` are configured, we will default to that list
+    # only. Otherwise, we will check if object's class includes Enumerable.
     def array_like?(object)
-      Blueprinter.configuration.array_like_classes.any? do |klass|
-        object.is_a?(klass)
+      if Blueprinter.configuration.array_like_classes.empty?
+        object.class.include?(Enumerable)
+      else
+        Blueprinter.configuration.array_like_classes.any? do |klass|
+          object.is_a?(klass)
+        end
       end
     end
   end

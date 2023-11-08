0bea7cbd665f326e9abec2df74e7d34c93219b47
Jordan Hollinger <jordan.hollinger@procore.com>
2023-11-08 18:40:40 -0500

    Add reflection on views, fields, and associations
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

---
+++ lib/blueprinter/reflection.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+module Blueprinter
+  #
+  # Public methods for reflecting on a Blueprint.
+  #
+  module Reflection
+    #
+    # Returns a Hash of views keyed by name.
+    #
+    # Example:
+    #
+    #   widget_view = WidgetBlueprint.views[:default]
+    #   category = widget_view.associations[:category]
+    #   category.blueprint
+    #   => CategoryBlueprint
+    #   category.view
+    #   => :default
+    #
+    # @return [Hash<Symbol, Blueprinter::Reflection::View>]
+    #
+    def views
+      @views ||= view_collection.views.transform_values do |view|
+        View.new(self, view)
+      end
+    end
+
+    #
+    # Represents a view within a Blueprint.
+    #
+    class View
+      Field = Struct.new(:name, :display_name, :options)
+      Association = Struct.new(:name, :display_name, :blueprint, :view, :options)
+
+      def initialize(blueprint, view)
+        @blueprint = blueprint
+        @view = view
+      end
+
+      # @return [String] The view's name
+      def name
+        @view.name
+      end
+
+      #
+      # Returns a Hash of fields in this view (recursive) keyed by method name.
+      #
+      # @return [Hash<Symbol, Blueprinter::Reflection::View::Field>]
+      #
+      def fields
+        @fields ||= @view.fields.each_with_object(included(:fields)) do |(_name, field), obj|
+          unless field.options[:association] # rubocop:todo Style/IfUnlessModifier
+            obj[field.method] = Field.new(field.method, field.name, field.options)
+          end
+        end
+      end
+
+      #
+      # Returns a Hash of associations in this view (recursive) keyed by method name.
+      #
+      # @return [Hash<Symbol, Blueprinter::Reflection::View::Association>]
+      #
+      def associations
+        @associations ||= @view.fields.each_with_object(included(:associations)) do |(_name, field), obj|
+          next unless field.options[:association]
+
+          blueprint = field.options.fetch(:blueprint)
+          view = field.options[:view] || :default
+          obj[field.method] = Association.new(field.method, field.name, blueprint, view, field.options)
+        end
+      end
+
+      private
+
+      # Recursively returns all fields or associations from included views
+      def included(type)
+        @view.included_view_names.reduce({}) do |acc, view_name|
+          view = @blueprint.views.fetch(view_name)
+          acc.merge view.send(type)
+        end
+      end
+    end
+  end
+end
---
+++ spec/units/reflection_spec.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require 'json'
+
+describe Blueprinter::Reflection do
+  let(:category_blueprint) {
+    Class.new(Blueprinter::Base) do
+      fields :id, :name
+    end
+  }
+
+  let(:part_blueprint) {
+    Class.new(Blueprinter::Base) do
+      fields :id, :name
+
+      view :extended do
+        include_view :default
+        field :description
+      end
+    end
+  }
+
+  let(:widget_blueprint) {
+    cat_bp = category_blueprint
+    part_bp = part_blueprint
+    Class.new(Blueprinter::Base) do
+      fields :id, :name
+      association :category, blueprint: cat_bp
+
+      view :extended do
+        include_view :default
+        association :parts, blueprint: part_bp, view: :extended
+      end
+
+      view :legacy do
+        include_view :default
+        association :parts, blueprint: part_bp, name: :pieces
+      end
+    end
+  }
+
+  it 'should list views' do
+    expect(widget_blueprint.views.keys).to eq [
+      :identifier,
+      :default,
+      :extended,
+      :legacy,
+    ]
+  end
+
+  it 'should list fields' do
+    expect(part_blueprint.views.fetch(:extended).fields.keys).to eq [
+      :id,
+      :name,
+      :description,
+    ]
+  end
+
+  it 'should list associations' do
+    associations = widget_blueprint.views.fetch(:default).associations
+    expect(associations.keys).to eq [:category]
+  end
+
+  it 'should list associations from included views' do
+    associations = widget_blueprint.views.fetch(:extended).associations
+    expect(associations.keys).to eq [:category, :parts]
+  end
+
+  it 'should list associations using custom names' do
+    associations = widget_blueprint.views.fetch(:legacy).associations
+    expect(associations.keys).to eq [:category, :parts]
+    expect(associations[:parts].display_name).to eq :pieces
+  end
+
+  it 'should get a blueprint and view from an association' do
+    assoc = widget_blueprint.views[:extended].associations[:parts]
+    expect(assoc.name).to eq :parts
+    expect(assoc.display_name).to eq :parts
+    expect(assoc.blueprint).to eq part_blueprint
+    expect(assoc.view).to eq :extended
+  end
+end

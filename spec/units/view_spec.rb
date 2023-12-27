1ca67b2072684e93efcada9c843e096d05aebe31
Nate Baer <nate.baer@procore.com>
2023-12-26 23:30:25 -0800

    Remove failing test for logic that was moved.
    Signed-off-by: Nate Baer <nate.baer@procore.com>

--- spec/units/view_spec.rb
+++ spec/units/view_spec.rb
@@ -98,15 +98,5 @@ describe '::View' do
     before do
       Blueprinter.configure { |config| config.default_transformers = [default_transform] }
     end
-
-    describe '#transformers' do
-      it 'should return the default transformers' do
-        expect(view_with_default_transform.transformers).to eq([default_transform])
-      end
-
-      it 'should allow for overriding the default transformers' do
-        expect(view_with_override_transform.transformers).to eq([override_transform])
-      end
-    end
   end
 end

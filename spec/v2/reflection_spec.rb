ca4cc256d6eadce1cf0c0050308e3434c4511da8
Jordan Hollinger <jordan.hollinger@procore.com>
2024-08-07 12:41:11 -0400

    Take a crack at fields and associations as structs
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- spec/v2/reflection_spec.rb
+++ spec/v2/reflection_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe "Blueprinter::V2 Reflection" do
-  it "should find all view names" do
+  it "should find all view keys" do
     blueprint = Class.new(Blueprinter::V2) do
       view :foo
       view :bar do
@@ -19,5 +19,45 @@ describe "Blueprinter::V2 Reflection" do
       bar.foo
       bar.foo.borp
     ).sort
+  end
+
+  it "should find all view names" do
+    blueprint = Class.new(Blueprinter::V2) do
+      view :foo
+      view :bar do
+        view :foo do
+          view :borp
+        end
+      end
+    end
+
+    views = blueprint.reflections.values
+    expect(views.map(&:name).sort).to eq %i(
+      default
+      foo
+      bar
+      bar.foo
+      bar.foo.borp
+    ).sort
+  end
+
+  it "should find fields and associations" do
+    category_blueprint = Class.new(Blueprinter::V2)
+    widget_blueprint = Class.new(Blueprinter::V2)
+    blueprint = Class.new(Blueprinter::V2) do
+      field :name
+      association :category, category_blueprint
+
+      view :extended do
+        field :description
+        association :widgets, widget_blueprint
+      end
+    end
+
+    expect blueprint.reflections[:default].fields.keys to eq %i(name)
+    expect blueprint.reflections[:default].associations.keys to eq %i(name)
+
+    expect blueprint.reflections[:extended].fields.keys to eq %i(name description)
+    expect blueprint.reflections[:extended].associations.keys to eq %i(name widgets)
   end
 end

919e56d6ca929c6f8383f64df51ae63086d55ced
Jordan Hollinger <jordan.hollinger@procore.com>
2024-08-24 12:45:10 -0400

    JIT evaluation of 'view', 'exclude', 'use', etc so that order doesn't matter
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- spec/v2/partials_spec.rb
+++ spec/v2/partials_spec.rb
@@ -18,24 +18,70 @@ describe "Blueprinter::V2 Partials" do
       end
     end
 
-    expect(blueprint.reflections[:default].fields.keys).to eq %i(name)
-    expect(blueprint.reflections[:foo].fields.keys.sort).to eq %i(
-      name
-      description
-    ).sort
-    expect(blueprint.reflections[:bar].fields.keys.sort).to eq %i(
-      name
-      description
-    ).sort
+    refs = blueprint.reflections
+    expect(refs[:default].fields.keys.sort).to eq %i(name).sort
+    expect(refs[:foo].fields.keys.sort).to eq %i(name description).sort
+    expect(refs[:bar].fields.keys.sort).to eq %i(name description).sort
+  end
+
+  it "should allow use statements to be nested" do
+    blueprint = Class.new(Blueprinter::V2) do
+      field :name
+      use :foo
+
+      partial :foo do
+        field :foo
+        use :bar
+      end
+
+      partial :bar do
+        field :bar
+        use :zorp
+      end
+
+      partial :zorp do
+        field :zorp
+      end
+    end
+
+    refs = blueprint.reflections
+    expect(refs[:default].fields.keys.sort).to eq %i(name foo bar zorp).sort
+  end
+
+  it "should allow a view to be defined in a partial" do
+    blueprint = Class.new(Blueprinter::V2) do
+      field :name
+
+      view :foo do
+        use :desc
+      end
+
+      view :bar do
+        use :desc
+      end
+
+      partial :desc do
+        field :description
+
+        view :extended do
+          field :long_description
+        end
+      end
+    end
+
+    refs = blueprint.reflections
+    expect(refs[:foo].fields.keys.sort).to eq %i(name description).sort
+    expect(refs[:bar].fields.keys.sort).to eq %i(name description).sort
+    expect(refs[:"foo.extended"].fields.keys.sort).to eq %i(name description long_description).sort
+    expect(refs[:"bar.extended"].fields.keys.sort).to eq %i(name description long_description).sort
   end
 
   it "should throw an error for an invalid partial name" do
-    expect do
-      Class.new(Blueprinter::V2) do
-        view :foo do
-          use :description
-        end
+    blueprint = Class.new(Blueprinter::V2) do
+      view :foo do
+        use :description
       end
-    end.to raise_error(Blueprinter::Errors::UnknownPartial)
+    end
+    expect { blueprint[:foo] }.to raise_error(Blueprinter::Errors::UnknownPartial)
   end
 end

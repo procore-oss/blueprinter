4cc0088f30a029f3da7e5764f92b9b445432ad11
Jordan Hollinger <jordan.hollinger@procore.com>
2024-11-01 10:33:53 -0400

    Field order extension for V2
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/v2/extensions.rb
+++ lib/blueprinter/v2/extensions.rb
@@ -4,6 +4,7 @@ module Blueprinter
   module V2
     module Extensions
       autoload :Exclusions, 'blueprinter/v2/extensions/exclusions'
+      autoload :FieldOrder, 'blueprinter/v2/extensions/field_order'
       autoload :Output, 'blueprinter/v2/extensions/output'
       autoload :Values, 'blueprinter/v2/extensions/values'
     end

5b70cbcaaeb335222be552485f43c618387656c6
Jordan Hollinger <jordan.hollinger@procore.com>
2024-11-01 08:10:34 -0400

    Class-based formatters for V2
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/v2.rb
+++ lib/blueprinter/v2.rb
@@ -8,6 +8,7 @@ module Blueprinter
     autoload :Base, 'blueprinter/v2/base'
     autoload :Context, 'blueprinter/v2/context'
     autoload :DSL, 'blueprinter/v2/dsl'
+    autoload :Formatter, 'blueprinter/v2/formatter'
     autoload :InstanceCache, 'blueprinter/v2/instance_cache'
     autoload :Reflection, 'blueprinter/v2/reflection'
     autoload :ViewBuilder, 'blueprinter/v2/view_builder'

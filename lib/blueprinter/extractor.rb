34532813df21c07a887b094cf336eb69ce252a03
Jordan Hollinger <jordan.hollinger@procore.com>
2024-11-01 08:18:46 -0400

    V2 Extractor
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/extractor.rb
+++ lib/blueprinter/extractor.rb
@@ -1,11 +1,36 @@
 # frozen_string_literal: true
 
 module Blueprinter
+  # The default extractor and base class for custom V2 extractors
   class Extractor
+    # Field extraction for V2
+    # @param ctx [Blueprinter::V2::Context]
+    def field(ctx)
+      if ctx.object.is_a? Hash
+        ctx.object[ctx.field.from] || ctx.object[ctx.field.from_str]
+      else
+        ctx.object.public_send(ctx.field.from)
+      end
+    end
+
+    # Object extraction for V2
+    # @param ctx [Blueprinter::V2::Context]
+    def object(ctx)
+      field ctx
+    end
+
+    # Collection extraction for V2
+    # @param ctx [Blueprinter::V2::Context]
+    def collection(ctx)
+      field ctx
+    end
+
+    # V1 extraction
     def extract(_field_name, _object, _local_options, _options = {})
       raise NotImplementedError, 'An Extractor must implement #extract'
     end
 
+    # V1 extraction
     def self.extract(field_name, object, local_options, options = {})
       new.extract(field_name, object, local_options, options)
     end
--- lib/blueprinter/v2.rb
+++ lib/blueprinter/v2.rb
@@ -6,6 +6,7 @@ module Blueprinter
   module V2
     autoload :Base, 'blueprinter/v2/base'
     autoload :DSL, 'blueprinter/v2/dsl'
+    autoload :Extensions, 'blueprinter/v2/extensions'
     autoload :Formatter, 'blueprinter/v2/formatter'
     autoload :Reflection, 'blueprinter/v2/reflection'
     autoload :ViewBuilder, 'blueprinter/v2/view_builder'

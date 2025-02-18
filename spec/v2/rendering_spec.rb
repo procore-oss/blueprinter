5e285a4520135d814e6bb21ea860a065f3424216
Jordan Hollinger <jordan.hollinger@procore.com>
2025-02-18 11:22:21 -0500

    Allow the 'view' option to work in V2...for now
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- spec/v2/rendering_spec.rb
+++ spec/v2/rendering_spec.rb
@@ -70,6 +70,26 @@ describe "Blueprinter::V2 Rendering" do
     })
   end
 
+  it 'should render an object with the deprecated view option' do
+    result = widget_blueprint.render_object(widget, { view: :extended }).to_hash
+    expect(result).to eq({
+      name: 'Foo',
+      description: 'About Foo',
+      cat: { name: 'Bar' },
+      parts: [{ num: 42 }, { num: 43 }]
+    })
+  end
+
+  it 'should render a collection with the deprecated view option' do
+    result = widget_blueprint.render_collection([widget], { view: :extended }).to_hash
+    expect(result).to eq([{
+      name: 'Foo',
+      description: 'About Foo',
+      cat: { name: 'Bar' },
+      parts: [{ num: 42 }, { num: 43 }]
+    }])
+  end
+
   it 'should use the same Context.store Hash throughout' do
     log_ext = Class.new(Blueprinter::Extension) do
       def initialize(log)

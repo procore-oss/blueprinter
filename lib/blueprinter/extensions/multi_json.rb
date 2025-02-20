639b003eefe9866d3ebe1645e96fe83bef9ff3b0
Jordan Hollinger <jordan.hollinger@procore.com>
2025-02-20 16:10:54 -0500

    MultiJson extension for V2
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

---
+++ lib/blueprinter/extensions/multi_json.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Blueprinter
+  module Extensions
+    #
+    # An optional, built-in extension for rendering JSON using the multi_json gem.
+    # Requires that you have the multi_json gem installed.
+    #
+    # Any options you pass to the extension's constructor will be passed as options to MultiJson.dump.
+    # You may also pass options through render/render_object/render_collection using the :multi_json key.
+    # They will be merged with any options passed to the extension's constructor.
+    #
+    class MultiJson < Extension
+      def initialize(options = {})
+        @options = options
+      end
+
+      # @param ctx [Blueprinter::V2::Context::Result]
+      def json(ctx)
+        opts = ctx.options[:multi_json] ? @options.merge(ctx.options[:multi_json]) : @options
+        ::MultiJson.dump(ctx.result, opts)
+      end
+    end
+  end
+end

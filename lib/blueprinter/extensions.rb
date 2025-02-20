7df91fc533bad26608588a4ee9dcfd54586caa25
Jordan Hollinger <jordan.hollinger@procore.com>
2025-02-20 16:10:54 -0500

    MultiJson extension for V2
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/extensions.rb
+++ lib/blueprinter/extensions.rb
@@ -4,5 +4,6 @@ module Blueprinter
   # Optional extensions for applications to pull in
   module Extensions
     autoload :FieldOrder, 'blueprinter/extensions/field_order'
+    autoload :MultiJson, 'blueprinter/extensions/multi_json'
   end
 end

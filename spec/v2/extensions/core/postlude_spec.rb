cc41af1ee3098202764c809be2fd0c448527afdf
Jordan Hollinger <jordan.hollinger@procore.com>
2025-02-20 15:14:42 -0500

    Root option for V2
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- spec/v2/extensions/core/postlude_spec.rb
+++ spec/v2/extensions/core/postlude_spec.rb
@@ -13,6 +13,70 @@ describe Blueprinter::V2::Extensions::Core::Postlude do
     end
   end
 
+  it 'passes through the result by default for objects' do
+    ctx = context.new(blueprint.new, nil, { name: 'Foo' }, nil, {})
+    result = subject.output_object(ctx)
+    expect(result).to eq({ name: 'Foo' })
+  end
+
+  it 'passes through the result by default for collections' do
+    ctx = context.new(blueprint.new, nil, [{ name: 'Foo' }], nil, {})
+    result = subject.output_collection(ctx)
+    expect(result).to eq([{ name: 'Foo' }])
+  end
+
+  it 'looks for a root option in the blueprint for objects' do
+    blueprint.options[:root] = :data
+    ctx = context.new(blueprint.new, nil, { name: 'Foo' }, nil, {})
+    result = subject.output_object(ctx)
+    expect(result).to eq({ data: { name: 'Foo' } })
+  end
+
+  it 'looks for a root option in the blueprint for collections' do
+    blueprint.options[:root] = :data
+    ctx = context.new(blueprint.new, nil, [{ name: 'Foo' }], nil, {})
+    result = subject.output_collection(ctx)
+    expect(result).to eq({ data: [{ name: 'Foo' }] })
+  end
+
+  it 'looks for a root option in the options over the blueprint' do
+    blueprint.options[:root] = :data
+    ctx = context.new(blueprint.new, nil, { name: 'Foo' }, nil, { root: :root })
+    result = subject.output_object(ctx)
+    expect(result).to eq({ root: { name: 'Foo' } })
+  end
+
+  it 'looks for a meta option in the blueprint' do
+    blueprint.options[:root] = :data
+    blueprint.options[:meta] = { links: [] }
+    ctx = context.new(blueprint.new, nil, { name: 'Foo' }, nil, {})
+    result = subject.output_object(ctx)
+    expect(result).to eq({ data: { name: 'Foo' }, meta: { links: [] } })
+  end
+
+  it 'looks for a meta option in the options over the blueprint' do
+    blueprint.options[:root] = :data
+    blueprint.options[:meta] = { links: [] }
+    ctx = context.new(blueprint.new, nil, { name: 'Foo' }, nil, { root: :root, meta: { linkz: [] }})
+    result = subject.output_object(ctx)
+    expect(result).to eq({ root: { name: 'Foo' }, meta: { linkz: [] } })
+  end
+
+  it 'looks for a meta Proc option in the blueprint' do
+    blueprint.options[:root] = :data
+    blueprint.options[:meta] = ->(ctx) { meta_links }
+    ctx = context.new(blueprint.new, nil, { name: 'Foo' }, nil, {})
+    result = subject.output_object(ctx)
+    expect(result).to eq({ data: { name: 'Foo' }, meta: { links: [] } })
+  end
+
+  it 'looks for a meta Proc option in the options' do
+    blueprint.options[:root] = :data
+    ctx = context.new(blueprint.new, nil, { name: 'Foo' }, nil, { root: :root, meta: ->(ctx) { meta_links } })
+    result = subject.output_object(ctx)
+    expect(result).to eq({ root: { name: 'Foo' }, meta: { links: [] } })
+  end
+
   it 'outputs json' do
     ctx = context.new(blueprint.new, nil, { name: 'Foo' }, nil, {})
     result = subject.json(ctx)

4b6dbbc2e1e6706a0797c7bd8ee434ba7d27f127
Jordan Hollinger <jordan.hollinger@procore.com>
2025-02-27 11:16:27 -0500

    DSL for defining an extension inline
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

---
+++ spec/v2/extension_dsl_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+describe "Blueprinter::V2 Extension DSL" do
+  let(:blueprint) do
+    blueprint = Class.new(Blueprinter::V2::Base) do
+      def self.blueprint_name = "NameBlueprint"
+      field :name
+      extension do
+        def field_value(ctx) = ctx.value.upcase
+      end
+      extension do
+        def blueprint_output(ctx) = { data: ctx.value }
+      end
+    end
+  end
+
+  it "defines multiple extensions" do
+    expect(blueprint.extensions.size).to eq 2
+    expect(blueprint.serializer.hooks.registered? :field_value).to be true
+    expect(blueprint.serializer.hooks.registered? :blueprint_output).to be true
+    expect(blueprint.serializer.hooks.registered? :object_value).to be false
+  end
+
+  it "names the extensions" do
+    expect(blueprint.extensions.map(&:class).map(&:name)).to eq ["NameBlueprint extension", "NameBlueprint extension"]
+  end
+
+  it "runs the extensions" do
+    res = blueprint.render({ name: "Foo" }).to_hash
+    expect(res).to eq({ data: { name: "FOO" } })
+  end
+end

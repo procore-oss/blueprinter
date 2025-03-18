17b8eb34eb1596d63199e2f6739b05db39c5b450
Jordan Hollinger <jordan.hollinger@procore.com>
2025-03-18 18:21:11 -0400

    Break Context into four parts
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/v2.rb
+++ lib/blueprinter/v2.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 require 'blueprinter/v2/fields'
+require 'blueprinter/v2/context'
 
 module Blueprinter
   module V2
     autoload :Base, 'blueprinter/v2/base'
+    autoload :Context, 'blueprinter/v2/context'
     autoload :DSL, 'blueprinter/v2/dsl'
     autoload :Reflection, 'blueprinter/v2/reflection'
     autoload :ViewBuilder, 'blueprinter/v2/view_builder'

709eb8843314113f9378954d57b06a986e2502cc
Jordan Hollinger <jordan.hollinger@procore.com>
2025-06-07 00:22:37 -0400

    Optimize InstanceCache
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- spec/extensions/hooks_spec.rb
+++ spec/extensions/hooks_spec.rb
@@ -55,7 +55,7 @@ describe Blueprinter::Hooks do
       exti1 = ext1.new
       exti2 = ext2.new
       hooks = described_class.new [exti1, exti2, Class.new(Blueprinter::Extension)]
-      ctx = render_ctx.new(instances[blueprint], {})
+      ctx = render_ctx.new(instances.blueprint(blueprint), {})
       hooks.run(:prepare, ctx)
       expect(exti1.log + exti2.log).to eq ['prepare', 'prepare']
     end
@@ -64,19 +64,19 @@ describe Blueprinter::Hooks do
   context '#any?' do
     it 'returns true if any hook returns true' do
       hooks = described_class.new [ext1.new, ext2.new]
-      ctx = field_ctx.new(instances[blueprint], {}, object, field, nil)
+      ctx = field_ctx.new(instances.blueprint(blueprint), {}, object, field, nil)
       expect(hooks.any?(:exclude_field?, ctx)).to be true
     end
 
     it 'returns false if no hooks return true' do
       hooks = described_class.new [ext1.new, ext2.new]
-      ctx = field_ctx.new(instances[blueprint], {}, object, field, 'Foo')
+      ctx = field_ctx.new(instances.blueprint(blueprint), {}, object, field, 'Foo')
       expect(hooks.any?(:exclude_field?, ctx)).to be false
     end
 
     it 'returns false if there are no extensions' do
       hooks = described_class.new []
-      ctx = field_ctx.new(instances[blueprint], {}, object, field, nil)
+      ctx = field_ctx.new(instances.blueprint(blueprint), {}, object, field, nil)
       expect(hooks.any?(:exclude_field?, ctx)).to be false
     end
   end
@@ -84,14 +84,14 @@ describe Blueprinter::Hooks do
   context '#first' do
     it 'returns the value from the first hook' do
       hooks = described_class.new [ext1.new, ext2.new]
-      ctx = field_ctx.new(instances[blueprint], {}, object, field, '')
+      ctx = field_ctx.new(instances.blueprint(blueprint), {}, object, field, '')
       result = hooks.first(:exclude_field?, ctx)
       expect(result).to be false
     end
 
     it 'returns nil if there are no hooks' do
       hooks = described_class.new []
-      ctx = field_ctx.new(instances[blueprint], {}, object, field, '')
+      ctx = field_ctx.new(instances.blueprint(blueprint), {}, object, field, '')
       result = hooks.first(:exclude_field?, ctx)
       expect(result).to be nil
     end
@@ -100,14 +100,14 @@ describe Blueprinter::Hooks do
   context '#last' do
     it 'returns the value from the last hook' do
       hooks = described_class.new [ext1.new, ext2.new]
-      ctx = field_ctx.new(instances[blueprint], {}, object, field, '')
+      ctx = field_ctx.new(instances.blueprint(blueprint), {}, object, field, '')
       result = hooks.last(:exclude_field?, ctx)
       expect(result).to be true
     end
 
     it 'returns nil if there are no hooks' do
       hooks = described_class.new []
-      ctx = field_ctx.new(instances[blueprint], {}, object, field, '')
+      ctx = field_ctx.new(instances.blueprint(blueprint), {}, object, field, '')
       result = hooks.last(:exclude_field?, ctx)
       expect(result).to be nil
     end
@@ -116,21 +116,21 @@ describe Blueprinter::Hooks do
   context '#reduce_hook' do
     it 'returns the final value' do
       hooks = described_class.new [ext1.new, ext2.new, ext1.new, ext1.new]
-      ctx = result_ctx.new(instances[blueprint], {}, object, { name: 'Foo', n: 0 })
+      ctx = result_ctx.new(instances.blueprint(blueprint), {}, object, { name: 'Foo', n: 0 })
       result = hooks.reduce_hook(:output_object, ctx.result) { |val| ctx.result = val; ctx }
       expect(result).to eq({ name: 'Foo', n: 3 })
     end
 
     it 'expands a returned array into args' do
       hooks = described_class.new [ext1.new, ext2.new, ext1.new, ext1.new]
-      ctx = result_ctx.new(instances[blueprint], {}, object, { name: 'Foo', n: 0 })
+      ctx = result_ctx.new(instances.blueprint(blueprint), {}, object, { name: 'Foo', n: 0 })
       result = hooks.reduce_hook(:output_object, ctx.result) { |val| ctx.result = val; [ctx] }
       expect(result).to eq({ name: 'Foo', n: 3 })
     end
 
     it 'returns the initial value if there are no hooks' do
       hooks = described_class.new []
-      ctx = result_ctx.new(instances[blueprint], {}, object, { name: 'Foo' })
+      ctx = result_ctx.new(instances.blueprint(blueprint), {}, object, { name: 'Foo' })
       result = hooks.reduce_hook(:output_object, ctx.result) { |val| ctx.result = val; ctx }
       expect(result).to eq({ name: 'Foo' })
     end
@@ -139,21 +139,21 @@ describe Blueprinter::Hooks do
   context '#reduce_into' do
     it 'returns the final value' do
       hooks = described_class.new [ext1.new, ext2.new, ext1.new, ext1.new]
-      ctx = result_ctx.new(instances[blueprint], {}, object, { name: 'Foo', n: 0 })
+      ctx = result_ctx.new(instances.blueprint(blueprint), {}, object, { name: 'Foo', n: 0 })
       result = hooks.reduce_into(:output_object, ctx, :result)
       expect(result).to eq({ name: 'Foo', n: 3 })
     end
 
     it 'expands a returned array into args' do
       hooks = described_class.new [ext1.new, ext2.new, ext1.new, ext1.new]
-      ctx = result_ctx.new(instances[blueprint], {}, object, { name: 'Foo', n: 0 })
+      ctx = result_ctx.new(instances.blueprint(blueprint), {}, object, { name: 'Foo', n: 0 })
       result = hooks.reduce_into(:output_object, ctx, :result)
       expect(result).to eq({ name: 'Foo', n: 3 })
     end
 
     it 'returns the initial value if there are no hooks' do
       hooks = described_class.new []
-      ctx = result_ctx.new(instances[blueprint], {}, object, { name: 'Foo' })
+      ctx = result_ctx.new(instances.blueprint(blueprint), {}, object, { name: 'Foo' })
       result = hooks.reduce_into(:output_object, ctx, :result)
       expect(result).to eq({ name: 'Foo' })
     end
@@ -230,10 +230,10 @@ describe Blueprinter::Hooks do
         log = []
         hooks = described_class.new [ext1.new(log), ext2.new(log)]
 
-        ctx = field_ctx.new(instances[blueprint], {}, object, field, 42)
+        ctx = field_ctx.new(instances.blueprint(blueprint), {}, object, field, 42)
         hooks.reduce_into(:field_value, ctx, :value)
 
-        ctx = object_ctx.new(instances[blueprint], {}, object)
+        ctx = object_ctx.new(instances.blueprint(blueprint), {}, object)
         hooks.around(:around_object_serialization, ctx) { log << 'INNER' }
 
         expect(log).to eq [
@@ -251,7 +251,7 @@ describe Blueprinter::Hooks do
       it 'is skipped for hidden extensions' do
         ext2.class_eval { def hidden? = true }
         log = []
-        ctx = field_ctx.new(instances[blueprint], {}, object, field, 42)
+        ctx = field_ctx.new(instances.blueprint(blueprint), {}, object, field, 42)
         hooks = described_class.new [ext1.new(log), ext2.new(log)]
 
         hooks.reduce_into(:field_value, ctx, :value)
@@ -270,7 +270,7 @@ describe Blueprinter::Hooks do
     it 'nests calls' do
       log = []
       extensions = [ext_a.new(log), ext_b.new(log), ext_c.new(log)]
-      ctx = object_ctx.new(instances[blueprint], {}, object)
+      ctx = object_ctx.new(instances.blueprint(blueprint), {}, object)
       def ctx.test_value = 42
       hooks = described_class.new extensions
       hooks.around(:around_object_serialization, ctx) { log << 'INNER' }
@@ -278,7 +278,7 @@ describe Blueprinter::Hooks do
     end
 
     it 'returns the inner value' do
-      ctx = object_ctx.new(instances[blueprint], {}, object)
+      ctx = object_ctx.new(instances.blueprint(blueprint), {}, object)
       def ctx.test_value = 42
       hooks = described_class.new [ext_a.new([]), ext_b.new([]), ext_c.new([])]
       result = hooks.around(:around_object_serialization, ctx) { 42 }
@@ -286,7 +286,7 @@ describe Blueprinter::Hooks do
     end
 
     it 'returns the inner with no hooks' do
-      ctx = object_ctx.new(instances[blueprint], {}, object)
+      ctx = object_ctx.new(instances.blueprint(blueprint), {}, object)
       hooks = described_class.new []
       result = hooks.around(:around_object_serialization, ctx) { 42 }
       expect(result).to eq 42
@@ -296,7 +296,7 @@ describe Blueprinter::Hooks do
       ext = Class.new(Blueprinter::Extension) do
         def around_object_serialization(_ctx); end
       end
-      ctx = object_ctx.new(instances[blueprint], {}, object)
+      ctx = object_ctx.new(instances.blueprint(blueprint), {}, object)
       hooks = described_class.new [ext.new]
       expect { hooks.around(:around_object_serialization, ctx) { 42 } }.to raise_error Blueprinter::BlueprinterError
     end
@@ -308,7 +308,7 @@ describe Blueprinter::Hooks do
           yield
         end
       end
-      ctx = object_ctx.new(instances[blueprint], {}, object)
+      ctx = object_ctx.new(instances.blueprint(blueprint), {}, object)
       hooks = described_class.new [ext.new]
       expect { hooks.around(:around_object_serialization, ctx) { 42 } }.to raise_error Blueprinter::BlueprinterError
     end

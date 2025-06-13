06a83f98e841437c34383372c0ee945d1bf01d7b
Jordan Hollinger <jordan.hollinger@procore.com>
2025-06-13 10:16:19 -0400

    Micro optimization to hooks
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/extension.rb
+++ lib/blueprinter/extension.rb
@@ -51,6 +51,11 @@ module Blueprinter
       pre_render
     ].freeze
 
+    # @return [Array<Symbol>] The names of the hooks implemented in this extension
+    def self.hooks
+      @_hooks ||= (public_instance_methods(true) & HOOKS).freeze
+    end
+
     # If this returns true, around_hook will not be called when this extension's hooks are run. Used by core extensions.
     def hidden? = false
 

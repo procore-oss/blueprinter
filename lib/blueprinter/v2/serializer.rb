01bf5e60c88c4a2116503cce4c5508e1b1aa7ac1
Jordan Hollinger <jordan.hollinger@procore.com>
2025-06-14 12:07:42 -0400

    Use instance vars in serializer hot paths
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/v2/serializer.rb
+++ lib/blueprinter/v2/serializer.rb
@@ -12,7 +12,7 @@ module Blueprinter
     # NOTE: The instance is re-used for the duration of the render.
     #
     class Serializer
-      attr_reader :blueprint, :fields, :field_serializers, :options, :instances, :formatter, :hooks, :defaults, :cond
+      attr_reader :blueprint, :fields, :options, :instances, :formatter, :hooks, :defaults, :cond
 
       # @param options [Hash] Options passed from the callsite
       def initialize(blueprint_class, options, instances, initial_depth:)
@@ -37,12 +37,12 @@ module Blueprinter
       #
       def object(object, depth:)
         if @run_around_serialize_object
-          ctx = Context::Object.new(blueprint, fields, options, object, depth)
-          hooks.around(:around_serialize_object, ctx) do
-            serialize_object(object, options, depth:)
+          ctx = Context::Object.new(@blueprint, @fields, @options, object, depth)
+          @hooks.around(:around_serialize_object, ctx) do
+            serialize_object(object, depth:)
           end
         else
-          serialize_object(object, options, depth:)
+          serialize_object(object, depth:)
         end
       end
 
@@ -54,61 +54,61 @@ module Blueprinter
       #
       def collection(collection, depth:)
         if @run_around_serialize_collection
-          ctx = Context::Object.new(blueprint, fields, options, collection, depth)
-          hooks.around(:around_serialize_collection, ctx) do
-            serialize_collection(collection, options, depth:)
+          ctx = Context::Object.new(@blueprint, @fields, @options, collection, depth)
+          @hooks.around(:around_serialize_collection, ctx) do
+            serialize_collection(collection, depth:)
           end
         else
-          serialize_collection(collection, options, depth:)
+          serialize_collection(collection, depth:)
         end
       end
 
       private
 
-      def serialize_object(object, options, depth:)
+      def serialize_object(object, depth:)
         if @run_object_input
-          ctx = Context::Object.new(blueprint, fields, options, object, depth)
-          object = hooks.reduce_into(:object_input, ctx, :object)
+          ctx = Context::Object.new(@blueprint, @fields, @options, object, depth)
+          object = @hooks.reduce_into(:object_input, ctx, :object)
         end
 
-        result = serialize(object, options, depth:)
+        result = serialize(object, depth:)
         return result unless @run_object_output
 
-        ctx = Context::Result.new(blueprint, fields, options, object, result, depth)
-        hooks.reduce_into(:object_output, ctx, :result)
+        ctx = Context::Result.new(@blueprint, @fields, @options, object, result, depth)
+        @hooks.reduce_into(:object_output, ctx, :result)
       end
 
-      def serialize_collection(collection, options, depth:)
+      def serialize_collection(collection, depth:)
         if @run_collection_input
-          ctx = Context::Object.new(blueprint, fields, options, collection, depth)
-          collection = hooks.reduce_into(:collection_input, ctx, :object)
+          ctx = Context::Object.new(@blueprint, @fields, @options, collection, depth)
+          collection = @hooks.reduce_into(:collection_input, ctx, :object)
         end
 
-        result = collection.map { |object| serialize(object, options, depth:) }.to_a
+        result = collection.map { |object| serialize(object, depth:) }.to_a
         return result unless @run_collection_output
 
-        ctx = Context::Result.new(blueprint, fields, options, collection, result, depth)
-        hooks.reduce_into(:collection_output, ctx, :result)
+        ctx = Context::Result.new(@blueprint, @fields, @options, collection, result, depth)
+        @hooks.reduce_into(:collection_output, ctx, :result)
       end
 
       # rubocop:disable Metrics/MethodLength
-      def serialize(object, options, depth:)
+      def serialize(object, depth:)
         if @run_blueprint_input
-          ctx = Context::Object.new(blueprint, fields, options, object, depth)
-          object = hooks.reduce_into(:blueprint_input, ctx, :object)
+          ctx = Context::Object.new(@blueprint, @fields, @options, object, depth)
+          object = @hooks.reduce_into(:blueprint_input, ctx, :object)
         end
 
-        ctx = Context::Field.new(blueprint, fields, options, object, nil, nil, depth)
-        result = field_serializers.each_with_object({}) do |field_conf, acc|
+        ctx = Context::Field.new(@blueprint, @fields, @options, object, nil, nil, depth)
+        result = @field_serializers.each_with_object({}) do |field_conf, acc|
           ctx.field = field_conf.field
           ctx.value = nil
-          ctx.value = ctx.field.value_proc ? proc_value(ctx) : hooks.call(field_conf.extractor, :extract_value, ctx)
+          ctx.value = ctx.field.value_proc ? proc_value(ctx) : @hooks.call(field_conf.extractor, :extract_value, ctx)
           field_conf.serialize(ctx, acc)
         end
 
         if @run_blueprint_output
-          ctx = Context::Result.new(blueprint, fields, options, object, result, depth)
-          hooks.reduce_into(:blueprint_output, ctx, :result)
+          ctx = Context::Result.new(@blueprint, @fields, @options, object, result, depth)
+          @hooks.reduce_into(:blueprint_output, ctx, :result)
         else
           result
         end
@@ -121,30 +121,30 @@ module Blueprinter
       end
 
       def extensions
-        extensions = blueprint.class.extensions.map { |ext| instances.extension ext }
+        extensions = @blueprint.class.extensions.map { |ext| @instances.extension ext }
         [Extensions::Core::Prelude.new, Extensions::Core::Extractor.new, *extensions, Extensions::Core::Postlude.new]
       end
 
       def blueprint_fields(depth)
-        default_fields = blueprint.class.reflections[:default].ordered
-        ctx = Context::Render.new(blueprint, default_fields, options, depth)
-        hooks.last(:blueprint_fields, ctx).freeze
+        default_fields = @blueprint.class.reflections[:default].ordered
+        ctx = Context::Render.new(@blueprint, default_fields, @options, depth)
+        @hooks.last(:blueprint_fields, ctx).freeze
       end
 
       # Allow extensions to do time-saving prep work on the current context
       def blueprint_setup(depth)
-        ctx = Context::Render.new(blueprint, fields, options, depth)
-        defaults.blueprint_setup ctx
-        cond.blueprint_setup ctx
-        hooks.run(:blueprint_setup, ctx)
+        ctx = Context::Render.new(@blueprint, @fields, @options, depth)
+        @defaults.blueprint_setup ctx
+        @cond.blueprint_setup ctx
+        @hooks.run(:blueprint_setup, ctx)
         setup_exts = {}.compare_by_identity
-        fields.map { |field| field_serializer(field, setup_exts, ctx) }.freeze
+        @fields.map { |field| field_serializer(field, setup_exts, ctx) }.freeze
       end
 
       # rubocop:disable Metrics/CyclomaticComplexity
       def field_serializer(field, setup_exts, ctx)
         ext = field.options[:extractor]
-        extractor = ext ? instances.extension(ext) : hooks.last_with(:extract_value)
+        extractor = ext ? @instances.extension(ext) : @hooks.last_with(:extract_value)
         setup_exts[extractor] ||= extractor.blueprint_setup(ctx) || true if extractor.respond_to?(:blueprint_setup)
 
         case field.type
@@ -160,14 +160,14 @@ module Blueprinter
 
       # We save a lot of time by skipping hooks that aren't used
       def find_used_hooks!
-        @run_around_serialize_object = hooks.registered? :around_serialize_object
-        @run_around_serialize_collection = hooks.registered? :around_serialize_collection
-        @run_object_input = hooks.registered? :object_input
-        @run_collection_input = hooks.registered? :collection_input
-        @run_blueprint_input = hooks.registered? :blueprint_input
-        @run_blueprint_output = hooks.registered? :blueprint_output
-        @run_object_output = hooks.registered? :object_output
-        @run_collection_output = hooks.registered? :collection_output
+        @run_around_serialize_object = @hooks.registered? :around_serialize_object
+        @run_around_serialize_collection = @hooks.registered? :around_serialize_collection
+        @run_object_input = @hooks.registered? :object_input
+        @run_collection_input = @hooks.registered? :collection_input
+        @run_blueprint_input = @hooks.registered? :blueprint_input
+        @run_blueprint_output = @hooks.registered? :blueprint_output
+        @run_object_output = @hooks.registered? :object_output
+        @run_collection_output = @hooks.registered? :collection_output
       end
     end
   end

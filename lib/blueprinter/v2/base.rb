a437f446b49977ef102b6a9968e9ea43a142fbc6
Jordan Hollinger <jordan.hollinger@procore.com>
2025-07-09 12:37:21 -0400

    Freeze extensions, options, and the schema when a blueprint is evaled
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/v2/base.rb
+++ lib/blueprinter/v2/base.rb
@@ -117,6 +117,14 @@ module Blueprinter
         end
 
         excludes.each { |f| schema.delete f }
+        extensions.freeze
+        options.freeze
+        schema.freeze
+        schema.each_value do |f|
+          f.options&.freeze
+          f.freeze
+        end
+
         @evaled = true
       end
 

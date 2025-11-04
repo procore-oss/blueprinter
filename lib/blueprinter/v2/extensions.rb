e9cbd4d4295c60ddadf7cf93be954b0c639d6527
Jordan Hollinger <jordan.hollinger@procore.com>
2025-11-04 12:46:21 -0500

    Clean up extraction code
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/v2/extensions.rb
+++ lib/blueprinter/v2/extensions.rb
@@ -7,7 +7,6 @@ module Blueprinter
       module Core
         autoload :Conditionals, 'blueprinter/v2/extensions/core/conditionals'
         autoload :Defaults, 'blueprinter/v2/extensions/core/defaults'
-        autoload :Extractor, 'blueprinter/v2/extensions/core/extractor'
         autoload :Json, 'blueprinter/v2/extensions/core/json'
         autoload :Wrapper, 'blueprinter/v2/extensions/core/wrapper'
       end

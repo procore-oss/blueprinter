cd5559e181725986b1f6fea1a29ba2456d26f1a4
Jordan Hollinger <jordan.hollinger@procore.com>
2025-11-16 22:29:56 -0500

    Clean up throw/catch
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- spec/v2/extensions/core/exclude_if_nil_spec.rb
+++ spec/v2/extensions/core/exclude_if_nil_spec.rb
@@ -3,60 +3,61 @@
 describe Blueprinter::V2::Extensions::Core::Conditionals do
   include ExtensionHelpers
   let(:object) { { foo: 'Foo' } }
-  let(:skip_field) { Blueprinter::V2::Serializer::SKIP }
+  let(:sig) { Blueprinter::V2::Serializer::SIGNAL }
+  let(:skip_field) { Blueprinter::V2::Serializer::SIG_SKIP }
 
   context 'fields' do
     let(:field) { blueprint.reflections[:default].fields[:foo] }
 
     it 'are allowed by default' do
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { 'Foo' } }
+      value = catch(sig) { subject.around_field_value(ctx) { 'Foo' } }
       expect(value).to eq 'Foo'
     end
 
     it 'are allowed by default if nil' do
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to be nil
     end
 
     it 'are allowed with options set' do
       ctx = prepare(blueprint, { exclude_if_nil: true }, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { 'Foo' } }
+      value = catch(sig) { subject.around_field_value(ctx) { 'Foo' } }
       expect(value).to eq 'Foo'
     end
 
     it 'are excluded with options set if nil' do
       ctx = prepare(blueprint, { exclude_if_nil: true }, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to eq skip_field
     end
 
     it 'are allowed with field options set' do
       blueprint.field :foo, exclude_if_nil: true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { 'Foo' } }
+      value = catch(sig) { subject.around_field_value(ctx) { 'Foo' } }
       expect(value).to eq 'Foo'
     end
 
     it 'are excluded with field options set if nil' do
       blueprint.field :foo, exclude_if_nil: true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to eq skip_field
     end
 
     it 'are allowed with blueprint options set' do
       blueprint.options[:exclude_if_nil] = true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { 'Foo' } }
+      value = catch(sig) { subject.around_field_value(ctx) { 'Foo' } }
       expect(value).to eq 'Foo'
     end
 
     it 'are excluded with blueprint options set if nil' do
       blueprint.options[:exclude_if_nil] = true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to eq skip_field
     end
   end
@@ -66,53 +67,53 @@ describe Blueprinter::V2::Extensions::Core::Conditionals do
 
     it 'are allowed by default' do
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { { name: 'Foo' } } }
+      value = catch(sig) { subject.around_field_value(ctx) { { name: 'Foo' } } }
       expect(value).to eq({ name: 'Foo' })
     end
 
     it 'are allowed by default if nil' do
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to be nil
     end
 
     it 'are allowed with options set' do
       ctx = prepare(blueprint, { exclude_if_nil: true }, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { { name: 'Foo' } } }
+      value = catch(sig) { subject.around_field_value(ctx) { { name: 'Foo' } } }
       expect(value).to eq({ name: 'Foo' })
     end
 
     it 'are excluded with options set if nil' do
       ctx = prepare(blueprint, { exclude_if_nil: true }, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to eq skip_field
     end
 
     it 'are allowed with field options set' do
       blueprint.object :foo_obj, sub_blueprint, exclude_if_nil: true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { { name: 'Foo' } } }
+      value = catch(sig) { subject.around_field_value(ctx) { { name: 'Foo' } } }
       expect(value).to eq({ name: 'Foo' })
     end
 
     it 'are excluded with field options set if nil' do
       blueprint.object :foo_obj, sub_blueprint, exclude_if_nil: true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to eq skip_field
     end
 
     it 'are allowed with blueprint options set' do
       blueprint.options[:exclude_if_nil] = true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { { name: 'Foo' } } }
+      value = catch(sig) { subject.around_field_value(ctx) { { name: 'Foo' } } }
       expect(value).to eq({ name: 'Foo' })
     end
 
     it 'are excluded with blueprint options set if nil' do
       blueprint.options[:exclude_if_nil] = true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to eq skip_field
     end
   end
@@ -122,53 +123,53 @@ describe Blueprinter::V2::Extensions::Core::Conditionals do
 
     it 'are allowed by default' do
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { [{ name: 'Foo' }] } }
+      value = catch(sig) { subject.around_field_value(ctx) { [{ name: 'Foo' }] } }
       expect(value).to eq([{ name: 'Foo' }])
     end
 
     it 'are allowed by default if nil' do
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to be nil
     end
 
     it 'are allowed with options set' do
       ctx = prepare(blueprint, { exclude_if_nil: true }, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { [{ name: 'Foo' }] } }
+      value = catch(sig) { subject.around_field_value(ctx) { [{ name: 'Foo' }] } }
       expect(value).to eq([{ name: 'Foo' }])
     end
 
     it 'are excluded with options set if nil' do
       ctx = prepare(blueprint, { exclude_if_nil: true }, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to eq skip_field
     end
 
     it 'are allowed with field options set' do
       blueprint.collection :foos, sub_blueprint, exclude_if_nil: true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { [{ name: 'Foo' }] } }
+      value = catch(sig) { subject.around_field_value(ctx) { [{ name: 'Foo' }] } }
       expect(value).to eq([{ name: 'Foo' }])
     end
 
     it 'are excluded with field options set if nil' do
       blueprint.collection :foos, sub_blueprint, exclude_if_nil: true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to eq skip_field
     end
 
     it 'are allowed with blueprint options set' do
       blueprint.options[:exclude_if_nil] = true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { [{ name: 'Foo' }] } }
+      value = catch(sig) { subject.around_field_value(ctx) { [{ name: 'Foo' }] } }
       expect(value).to eq([{ name: 'Foo' }])
     end
 
     it 'are excluded with blueprint options set if nil' do
       blueprint.options[:exclude_if_nil] = true
       ctx = prepare(blueprint, {}, Blueprinter::V2::Context::Field, object, field)
-      value = catch(skip_field) { subject.around_field_value(ctx) { nil } }
+      value = catch(sig) { subject.around_field_value(ctx) { nil } }
       expect(value).to eq skip_field
     end
   end

504372efcc40f368a7fb853fc2b708946c70d835
Jacob Sheehy <jacobjsheehy@gmail.com>
2026-01-06 14:15:26 -0500

    [chore] Unwind method signature change (again)
    Signed-off-by: Jacob Sheehy <jacobjsheehy@gmail.com>

--- lib/blueprinter/extractors/hash_extractor.rb
+++ lib/blueprinter/extractors/hash_extractor.rb
@@ -7,7 +7,7 @@ module Blueprinter
   class HashExtractor < Extractor
     # @param field_name [Symbol] The name of the field to extract
     # @param object [Hash] The Hash object in which the field_name is extracted from
-    def extract(field_name, object, _local_options = {}, _options = {})
+    def extract(field_name, object, _local_options, _options = {})
       object[field_name]
     end
   end
--- lib/blueprinter/extractors/public_send_extractor.rb
+++ lib/blueprinter/extractors/public_send_extractor.rb
@@ -7,7 +7,7 @@ module Blueprinter
   class PublicSendExtractor < Extractor
     # @param field_name [Symbol] The name of the field to extract
     # @param object [Object] The object in which the field_name is sent to for extraction
-    def extract(field_name, object, _local_options = {}, _options = {})
+    def extract(field_name, object, _local_options, _options = {})
       object.public_send(field_name)
     end
   end

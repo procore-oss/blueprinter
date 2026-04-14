b0a3fae76a7bde0a5ceba1cf9c5a9451db9680ab
jhollinger <jhollinger@users.noreply.github.com>
2026-04-14 20:29:40 +0000

    deploy: d7c5948497e55ce51786850c26886d13aed814a3

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f04616ff.js',
+                path_to_root + 'searchindex-d0a84f19.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');

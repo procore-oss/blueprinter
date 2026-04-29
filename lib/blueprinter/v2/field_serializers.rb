2d76196230f73ea512951f130eb2fb892c5bca6b
Jordan Hollinger <jordan.hollinger@procore.com>
2026-04-29 16:17:11 -0400

    Centralize incrementing serialization depth
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/v2/field_serializers.rb
+++ lib/blueprinter/v2/field_serializers.rb
@@ -7,21 +7,21 @@ module Blueprinter
       # Serializer for V2 objects
       module Object
         def self.serialize(blueprint_class, value, options, parent:, instances:, store:, depth:)
-          blueprint_class.serializer.object(value, options, parent:, instances:, store:, depth: depth + 1)
+          blueprint_class.serializer.object(value, options, parent:, instances:, store:, depth:)
         end
       end
 
       # Serializer for V2 collections
       module Collection
         def self.serialize(blueprint_class, value, options, parent:, instances:, store:, depth:)
-          blueprint_class.serializer.collection(value, options, parent:, instances:, store:, depth: depth + 1)
+          blueprint_class.serializer.collection(value, options, parent:, instances:, store:, depth:)
         end
       end
 
       # Serializer for V1 associations
       module V1Association
         def self.serialize(blueprint_class, value, options, parent:, instances:, store:, depth:)
-          opts = { v2_instances: instances, v2_depth: depth + 1, v2_store: store }
+          opts = { v2_instances: instances, v2_depth: depth, v2_store: store }
           blueprint_class.hashify(value, view_name: :default, local_options: options.dup.merge(opts))
         end
       end

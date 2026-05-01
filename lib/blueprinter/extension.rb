659b7e1a099e299098eff3cb9cccc246914a0a4a
Jordan Hollinger <jordan.hollinger@procore.com>
2026-05-01 09:29:20 -0400

    Move ExtensionHelpers back into Extension since it's only used in one place now
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/extension.rb
+++ lib/blueprinter/extension.rb
@@ -17,8 +17,6 @@ module Blueprinter
   #  - pre_render
   #
   class Extension
-    include V2::ExtensionHelpers
-
     HOOKS = %i[
       around_hook
       around_result
@@ -39,45 +37,8 @@ module Blueprinter
     # If this returns true, around_hook will not be called when this extension's hooks are run. Used by core extensions.
     def hidden? = false
 
-    # around_result TODO
-    # @param context [Blueprinter::V2::Context::Result]
-
-    # around_serialize_object: Runs around serialization of a Blueprint object.
-    # @param context [Blueprinter::V2::Context::Object]
-
-    # around_serialize_collection: Runs around serialization of a Blueprint collection.
-    # @param context [Blueprinter::V2::Context::Object]
-
-    # around_field_value TODO
-
-    # around_object_value TODO
-
-    # around_collection_value TODO
-
-    # blueprint_fields: Returns the fields that should be included in the correct order. Default is all fields in the order
-    # in which they were defined.
-    # NOTE If there are multiple blueprint_fields hooks, only the last one is called.
-    # NOTE Only runs once per Blueprint per render.
-    # @param context [Blueprinter::V2::Context::Init]
-    # @return [Array<Blueprinter::V2::Fields::Field|Blueprinter::V2::Fields::Object|Blueprinter::V2::Fields::Collection>]
-
-    # blueprint_setup: Called once per blueprint per render. A common use is to pre-calculate certain options
-    # and cache them in context.data, so we don't have to recalculate them for every field.
-    # @param context [Blueprinter::V2::Context::Init]
-
-    # around_hook: Instrument extension hook calls. MUST yield!
-    # @param extension [Blueprinter::Extension] Instance of the extension
-    # @param hook [Symbol] Name of hook being called
-
-    # pre_render: Called eary during "render" in V1, this method receives the object to be rendered and
-    # may return a modified (or new) object to be rendered.
-    # @param object [Object] The object to be rendered
-    # @param blueprint [Class] The Blueprinter class
-    # @param view [Symbol] The blueprint view
-    # @param options [Hash] Options passed to "render"
-    # @return [Object] The object to continue rendering
-
-    private
+    # Skip the current field and halt further field hooks
+    def skip! = throw V2::Serializer::SIGNAL, V2::Serializer::SIG_SKIP
 
     # Helper for around_result hooks to declare that a result is "final"
     def final(val) = V2::Context::Final.new(val)

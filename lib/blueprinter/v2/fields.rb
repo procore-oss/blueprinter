23db99f1abd9e1fbd4f348dbb6bd469f54c3b7a5
Jordan Hollinger <jordan.hollinger@procore.com>
2026-05-05 15:30:20 -0400

    Put all the field classes under a module
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/v2/fields.rb
+++ lib/blueprinter/v2/fields.rb
@@ -2,117 +2,120 @@
 
 module Blueprinter
   module V2
-    # Common methods for Field and ConfigurableField
-    module FieldHelpers
-      # @return [true | false] Returns true if this is a regular field
-      def field? = type == :field
+    # Representaions of declared fields
+    module Fields
+      # Common methods for Field and Configurable
+      module Helpers
+        # @return [true | false] Returns true if this is a regular field
+        def field? = type == :field
 
-      # @return [true | false] Returns true if this is an object field
-      def object? = type == :object
+        # @return [true | false] Returns true if this is an object field
+        def object? = type == :object
 
-      # @return [true | false] Returns true if this is a collection field
-      def collection? = type == :collection
+        # @return [true | false] Returns true if this is a collection field
+        def collection? = type == :collection
 
-      # @return [true | false] Returns true if this is an object or collection field
-      def association? = object? || collection?
-    end
-
-    # The internal representation of a field or association. Also exposed via reflections.
-    #
-    # @!attribute [r] type
-    #   @return [:field | :object | :collection] The type of field
-    # @!attribute [r] name
-    #   @return [Symbol] Name of field in result
-    # @!attribute [r] source
-    #   @return [Symbol] Method name/Hash key to pull the field value from
-    # @!attribute [r] source_str
-    #   @return [String] Same as `source` but a string
-    # @!attribute [r] options
-    #   @return [Hash] Options defined on the field
-    # @!attribute [r] value_proc
-    #   @return [Proc | nil] A proc to extract the value
-    # @!attribute [r] blueprint
-    #   @return [Class | nil] Blueprint to serialize with (objects and collections only)
-    Field = Struct.new(
-      :type,
-      :name,
-      :source,
-      :source_str,
-      :options,
-      :value_proc,
-      :blueprint,
-      :_merged_options,
-      :_has_conditional,
-      :_has_default,
-      :_extractor,
-      :_serializer,
-      keyword_init: true
-    ) do
-      include FieldHelpers
-
-      # Returns a copy of this field that extensions can modify
-      # @!visibility private
-      def to_configurable
-        ConfigurableField.new(type, name, source, options.dup, value_proc, blueprint, self)
-      end
-    end
-
-    # Representation of a field that's modifiable inside `around_blueprint_init` hooks.
-    #
-    # @!attribute [r] type
-    #   @return [:field | :object | :collection] The type of field
-    # @!attribute [rw] name
-    #   @return [Symbol] Name of field in result
-    # @!attribute [rw] source
-    #   @return [Symbol] Method name/Hash key to pull the field value from
-    # @!attribute [rw] options
-    #   @return [Hash] Options defined on the field
-    # @!attribute [rw] value_proc
-    #   @return [Proc | nil] A proc to extract the value
-    # @!attribute [r] blueprint
-    #   @return [Class | nil] Blueprint to serialize with (objects and collections only)
-    ConfigurableField = Struct.new(:type, :name, :source, :options, :value_proc, :blueprint, :_original) do
-      include FieldHelpers
-
-      # Remove setters from field that shouldn't be changed
-      static_members = %i[type blueprint _original]
-      dynamic_members = members - static_members
-      static_members.each { |member| remove_method "#{member}=" }
-
-      # @!visibility private
-      def changed?
-        @changed || options != _original.options
+        # @return [true | false] Returns true if this is an object or collection field
+        def association? = object? || collection?
       end
 
-      # @!visibility private
-      def to_internal
-        Field.new(
-          type:,
-          name: name.to_sym,
-          source: source.to_sym,
-          source_str: source == _original.source ? _original.source_str : source.to_s,
-          options:,
-          value_proc:,
-          blueprint:
-        )
+      # The internal representation of a field or association. Also exposed via reflections.
+      #
+      # @!attribute [r] type
+      #   @return [:field | :object | :collection] The type of field
+      # @!attribute [r] name
+      #   @return [Symbol] Name of field in result
+      # @!attribute [r] source
+      #   @return [Symbol] Method name/Hash key to pull the field value from
+      # @!attribute [r] source_str
+      #   @return [String] Same as `source` but a string
+      # @!attribute [r] options
+      #   @return [Hash] Options defined on the field
+      # @!attribute [r] value_proc
+      #   @return [Proc | nil] A proc to extract the value
+      # @!attribute [r] blueprint
+      #   @return [Class | nil] Blueprint to serialize with (objects and collections only)
+      Field = Struct.new(
+        :type,
+        :name,
+        :source,
+        :source_str,
+        :options,
+        :value_proc,
+        :blueprint,
+        :_merged_options,
+        :_has_conditional,
+        :_has_default,
+        :_extractor,
+        :_serializer,
+        keyword_init: true
+      ) do
+        include Helpers
+
+        # Returns a copy of this field that extensions can modify
+        # @!visibility private
+        def to_configurable
+          Configurable.new(type, name, source, options.dup, value_proc, blueprint, self)
+        end
       end
 
-      # rubocop:disable Lint/UselessAccessModifier
+      # Representation of a field that's modifiable inside `around_blueprint_init` hooks.
+      #
+      # @!attribute [r] type
+      #   @return [:field | :object | :collection] The type of field
+      # @!attribute [rw] name
+      #   @return [Symbol] Name of field in result
+      # @!attribute [rw] source
+      #   @return [Symbol] Method name/Hash key to pull the field value from
+      # @!attribute [rw] options
+      #   @return [Hash] Options defined on the field
+      # @!attribute [rw] value_proc
+      #   @return [Proc | nil] A proc to extract the value
+      # @!attribute [r] blueprint
+      #   @return [Class | nil] Blueprint to serialize with (objects and collections only)
+      Configurable = Struct.new(:type, :name, :source, :options, :value_proc, :blueprint, :_original) do
+        include Helpers
 
-      private
+        # Remove setters from field that shouldn't be changed
+        static_members = %i[type blueprint _original]
+        dynamic_members = members - static_members
+        static_members.each { |member| remove_method "#{member}=" }
 
-      # rubocop:enable Lint/UselessAccessModifier
+        # @!visibility private
+        def changed?
+          @changed || options != _original.options
+        end
 
-      dynamic_members.each do |member|
-        alias_method :"set_#{member}", :"#{member}="
-      end
+        # @!visibility private
+        def to_internal
+          Field.new(
+            type:,
+            name: name.to_sym,
+            source: source.to_sym,
+            source_str: source == _original.source ? _original.source_str : source.to_s,
+            options:,
+            value_proc:,
+            blueprint:
+          )
+        end
 
-      public
+        # rubocop:disable Lint/UselessAccessModifier
 
-      dynamic_members.each do |member|
-        define_method :"#{member}=" do |val|
-          @changed = true
-          send(:"set_#{method}", val)
+        private
+
+        # rubocop:enable Lint/UselessAccessModifier
+
+        dynamic_members.each do |member|
+          alias_method :"set_#{member}", :"#{member}="
+        end
+
+        public
+
+        dynamic_members.each do |member|
+          define_method :"#{member}=" do |val|
+            @changed = true
+            send(:"set_#{method}", val)
+          end
         end
       end
     end

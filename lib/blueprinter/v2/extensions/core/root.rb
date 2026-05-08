e62dfac25d8071006ac740440b57137cfda15f57
Jordan Hollinger <jordan.hollinger@procore.com>
2026-05-08 15:34:08 -0400

    Rename the core Wrapper extension to Root.
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

---
+++ lib/blueprinter/v2/extensions/core/root.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Blueprinter
+  module V2
+    module Extensions
+      module Core
+        #
+        # A core extension for wrapping the result with metadata.
+        #
+        class Root < Extension
+          # @param ctx [Blueprinter::V2::Context::Result]
+          def around_result(ctx)
+            result = yield ctx
+            root_name = ctx.options[:root]
+            return result if final?(result) || !root_name
+
+            root = { root_name => result }
+            root[:meta] = ctx.options[:meta] if ctx.options[:meta]
+            root
+          end
+
+          def hidden? = true
+        end
+      end
+    end
+  end
+end

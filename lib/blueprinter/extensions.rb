f0d78a59165f999415d6d866744cadb70f69d0ca
Jordan Hollinger <jordan.hollinger@procore.com>
2026-05-10 23:17:21 -0400

    Extension to make V1's 'extractor' option work
    Signed-off-by: Jordan Hollinger <jordan.hollinger@procore.com>

--- lib/blueprinter/extensions.rb
+++ lib/blueprinter/extensions.rb
@@ -6,6 +6,7 @@ module Blueprinter
     autoload :FieldOrder, 'blueprinter/extensions/field_order'
     autoload :LegacyConditionals, 'blueprinter/extensions/legacy_conditionals'
     autoload :LegacyDefaultIf, 'blueprinter/extensions/legacy_default_if'
+    autoload :LegacyExtractorOption, 'blueprinter/extensions/legacy_extractor_option'
     autoload :LegacyRenameField, 'blueprinter/extensions/legacy_rename_field'
     autoload :MultiJson, 'blueprinter/extensions/multi_json'
     autoload :OpenTelemetry, 'blueprinter/extensions/open_telemetry'
